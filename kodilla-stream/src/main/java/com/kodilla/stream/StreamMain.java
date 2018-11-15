5b9273298e115a5bfffda82e6e1372d648d2b64f
fmatusiak <f.matusiak95@gmail.com>
2018-11-15 19:26:45 +0100

    add Forum and ForumUser class

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,14 +2,29 @@ package com.kodilla.stream;
 
 
 import com.kodilla.stream.beautifer.PoemBeautifer;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
 import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.person.People;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
 import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
+        Forum forum = new Forum();
+        Map<Integer,ForumUser> forumUsers = forum.getUserList().stream().filter(c-> c.getSex() == 'M').filter(c->c.getBirthdayDate().getYear() <= 1998).filter(c->c.getCountPosts() >= 1).collect(Collectors.toMap(ForumUser::getId,ForumUser -> ForumUser));
+        System.out.println(forumUsers);
+
+
+
         /*//System.out.println("Welcome to module 7 - Stream");
         Processor processor = new Processor();
         Executor codeToExecute = () -> System.out.println("This is example text.");
@@ -27,15 +42,44 @@ public class StreamMain {
         expressionExecutor.executeExpression(20, 5,FunctionalCalculator::subBFromA);
         expressionExecutor.executeExpression(50, 10,FunctionalCalculator::divideAByB);
 */
-        PoemBeautifer poemBeautifer = new PoemBeautifer();
+       /* PoemBeautifer poemBeautifer = new PoemBeautifer();
         poemBeautifer.beautify("Edward", (decorate -> decorate.toUpperCase()));
         poemBeautifer.beautify("janek", (decorate -> decorate.toLowerCase()));
         poemBeautifer.beautify("zenek", decorate -> decorate + "ABC");
         poemBeautifer.beautify("marian", decorate -> "$$$ " + decorate + " @@");
 
         System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+        NumbersGenerator.generateEven(20);*/
 
+        /*People.getList().stream().forEach(System.out::println);
+        People.getList().stream().map(s->s.toUpperCase()).forEach(System.out::println);
+        People.getList().stream().filter(s->s.length() > 11).forEach(System.out::println);
+        People.getList().stream().map(s->s.toUpperCase()).filter(s -> s.length() > 11).map(s->s.substring(0,s.indexOf(" ") + 2) + ".").filter(s->s.substring(0,1).equals("M")).forEach(System.out::println);
+*/
+       /* BookDirectory theBookDirectory = new BookDirectory();
+        theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .forEach(System.out::println);*/
+
+        /*BookDirectory theBookDirectory = new BookDirectory();
+        List<Book> theResultListOfBooks = theBookDirectory.getList().stream().filter(book -> book.getYearOfPublication() > 2005).collect(Collectors.toList());
+        System.out.println("# elements: " + theResultListOfBooks.size());
+        theResultListOfBooks.stream().forEach(System.out::println);*/
+
+        /*BookDirectory theBookDirectory = new BookDirectory();
+
+        Map<String, Book> theResultMapOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toMap(Book::getSignature, book -> book));
+
+        System.out.println("# elements: " + theResultMapOfBooks.size());
+        theResultMapOfBooks.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);*/
+
+       /* BookDirectory theBookDirectory = new BookDirectory();
+        String theResultStringOfBooks = theBookDirectory.getList().stream().filter(book -> book.getYearOfPublication() > 2005).map(Book::toString).collect(Collectors.joining(",\n","<<",">>"));
+        System.out.println(theResultStringOfBooks);*/
 
     }
 }
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/forumuser/ForumUser.java
@@ -0,0 +1,40 @@
+package com.kodilla.stream.forumuser;
+
+import java.time.LocalDate;
+
+public final class ForumUser {
+    private final int id;
+    private final String userName;
+    private final LocalDate birthdayDate;
+    private final int countPosts;
+    private char sex;
+
+
+    public ForumUser(final int id,final String userName,final LocalDate birthdayDate,final int countPosts,char sex) {
+        this.id = id;
+        this.userName = userName;
+        this.birthdayDate = birthdayDate;
+        this.countPosts = countPosts;
+        this.sex = sex;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public LocalDate getBirthdayDate() {
+        return birthdayDate;
+    }
+
+    public int getCountPosts() {
+        return countPosts;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+}
