899e799a7ba1eab417ddf36b7bd8a41110b44fc2
fmatusiak <f.matusiak95@gmail.com>
2018-11-21 13:44:16 +0100

    modifed FirstChallenge

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -1,26 +1,23 @@
 package com.kodilla.exception.test;
 
 public class FirstChallenge {
-    public double divide(double a, double b) {
-        double resultsDivide = 0;
-        try {
-            if(b == 0)
-                throw new ArithmeticException();
-
-                resultsDivide = a / b;
-
-        } catch (ArithmeticException e) {
-            System.out.println("Can't divide by ZERO");
-        } finally {
-            System.out.println(" #### The task was completed #### ");
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException();
         }
-
-        return resultsDivide;
+        return a / b;
     }
 
     public static void main(String[] args) {
         FirstChallenge firstChallenge = new FirstChallenge();
-        double result = firstChallenge.divide(3, 0);
-        System.out.println(result);
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Cant divide for ZERO");
+        } finally {
+            System.out.println("END TASK");
+        }
+
     }
 }
