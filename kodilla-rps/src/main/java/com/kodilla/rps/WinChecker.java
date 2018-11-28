1913a10491b451481ec228ea73e90e74d98fcda6
fmatusiak <f.matusiak95@gmail.com>
2018-11-28 14:55:09 +0100

    Correction WinChecker

--- kodilla-rps/src/main/java/com/kodilla/rps/WinChecker.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/WinChecker.java
@@ -7,18 +7,18 @@ public class WinChecker {
         if (moveUser.equals(moveComputer)) {
             return 0;
 
-        }  else if ((moveUser.equals("1") && moveComputer.equals("3")) || (moveUser.equals("2") && moveComputer.equals("1"))
+        } else if ((moveUser.equals("1") && moveComputer.equals("3")) || (moveUser.equals("2") && moveComputer.equals("1"))
                 || (moveUser.equals("3") && moveComputer.equals("2")) || (moveUser.equals("1") && moveComputer.equals("4"))
                 || (moveUser.equals("4") && moveComputer.equals("5")) || (moveUser.equals("5") && moveComputer.equals("3"))
                 || (moveUser.equals("3") && moveComputer.equals("4")) || (moveUser.equals("4") && moveComputer.equals("2"))
-                || (moveUser.equals("2") && moveComputer.equals("5")) || (moveUser.equals("5") && moveComputer.equals("1")) ) {
+                || (moveUser.equals("2") && moveComputer.equals("5")) || (moveUser.equals("5") && moveComputer.equals("1"))) {
             return 1;
 
         } else if ((moveUser.equals("5") && moveComputer.equals("4")) || (moveUser.equals("3") && moveComputer.equals("5"))
                 || (moveUser.equals("4") && moveComputer.equals("3")) || (moveUser.equals("2") && moveComputer.equals("4"))
                 || (moveUser.equals("5") && moveComputer.equals("2")) || (moveUser.equals("4") && moveComputer.equals("1"))
                 || (moveUser.equals("1") && moveComputer.equals("5")) || moveUser.equals("1") && moveComputer.equals("2")
-                || (moveUser.equals("2") && moveComputer.equals("3")) || (moveUser.equals("3") && moveComputer.equals("1"))  ){
+                || (moveUser.equals("2") && moveComputer.equals("3")) || (moveUser.equals("3") && moveComputer.equals("1"))) {
             return 2;
         }
 
