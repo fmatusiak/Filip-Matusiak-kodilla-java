ab4427acabac11bc2a7b0cebb729f29b6542d4a5
fmatusiak <f.matusiak95@gmail.com>
2019-05-14 01:36:56 +0200

    add EbayTestingApp

--- kodilla-testing/src/test/java/com/kodilla/testing/loop/testLoopAndConditionalBreakpoints.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/loop/testLoopAndConditionalBreakpoints.java
@@ -6,16 +6,16 @@ import org.junit.Test;
 public class testLoopAndConditionalBreakpoints {
 
     @Test
-    public void testLoop(){
+    public void testLoop() {
         //given
         long sum = 0;
         //when
-        for(int i = 0; i < 1000;i++){
-            sum+=i;
+        for (int i = 0; i < 1000; i++) {
+            sum += i;
             System.out.println("[" + i + "]" + sum);
         }
         //then
-        Assert.assertEquals(499500,sum);
+        Assert.assertEquals(499500, sum);
     }
 
 }
--- kodilla-testing2/src/main/java/com/kodilla/testing2/EbayTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/EbayTestingApp.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class EbayTestingApp {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         WebDriver webDriver = WebDriverConfig.getDriver(WebDriverConfig.FIREFOX);
         webDriver.get("https://www.ebay.com/");
 
